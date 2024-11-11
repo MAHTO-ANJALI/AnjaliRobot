674e5a216bd69ab67a921600093a7194453cf2ee
[🇮🇳]˼ Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ ˹♡︎ـﮩ🫧 <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-11-11 09:10:39 +0530

    Update ping.py

--- AvishaRobot/modules/ping.py
+++ AvishaRobot/modules/ping.py
@@ -82,7 +82,7 @@ def ping(update: Update, context: CallbackContext):
         "𖣐 ʟ ᴜ ᴄ ʏ • / ‹𝟹 ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ ♥︎\n\n"
         "● <b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ ➥</b> <code>{}</code>\n"
         "● <b>ᴜᴘᴛɪᴍᴇ ➥</b> <code>{}</code>\n\n"
-        "𖣐 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ʟ ᴜ ᴄ ʏ • / ‹𝟹".format(telegram_ping, uptime),
+        "𖣐 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳]".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
